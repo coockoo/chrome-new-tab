5f20c7fc24bbc82bbd731cc7b2e974e5e0d91240
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2020-09-23 20:34:36 +0300

    Add initial app scaffold

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+function App() {
+  return <div>Kappa</div>;
+}
+
+export default hot(App);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HTMLPlugin = require('html-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
+const isDev = mode === 'development';
 
 module.exports = {
   mode,
@@ -11,6 +13,45 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
+  },
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, './src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['react-hot-loader/babel'],
+          },
+        },
+      },
+
+      {
+        test: /\.less$/,
+        include: path.join(__dirname, './src'),
+        use: [
+          isDev ? { loader: 'style-loader' } : { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+              importLoaders: 1,
+            },
+          },
+          { loader: 'less-loader' },
+        ],
+      },
+    ],
   },
   plugins: [
     new HTMLPlugin({
@@ -24,6 +65,9 @@ module.exports = {
           to: path.resolve(__dirname, './dist/manifest.json'),
         },
       ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
     }),
   ],
 };
